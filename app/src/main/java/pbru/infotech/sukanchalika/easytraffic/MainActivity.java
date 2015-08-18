e1fefb5f9b754834984cae94177783cd99459949
Sukan <ajsukan@gmail.com>
2015-08-18 13:47:40 +0700

    after lunch

--- app/src/main/java/pbru/infotech/sukanchalika/easytraffic/MainActivity.java
+++ app/src/main/java/pbru/infotech/sukanchalika/easytraffic/MainActivity.java
@@ -40,13 +40,25 @@ public class MainActivity extends AppCompatActivity {
         strTitle[10] = "ทางแยก";
         strTitle[11] = "ห้ามกลับรถ";
         strTitle[12] = "ห้ามจอด";
-        strTitle[13] = "ระวังรถสวนทาง";
+        strTitle[13] = "ระวังรถสวน";
         strTitle[14] = "ห้ามแซง";
         strTitle[15] = "เข้า";
         strTitle[16] = "หยุดตรวจ";
         strTitle[17] = "จำกัดความเร็ว";
         strTitle[18] = "จำกัดความกว้าง";
         strTitle[19] = "จำกัดความสูง";
+
+        int[] intImage = {R.drawable.traffic_01, R.drawable.traffic_02,
+                R.drawable.traffic_03, R.drawable.traffic_04, R.drawable.traffic_05,
+                R.drawable.traffic_06, R.drawable.traffic_07, R.drawable.traffic_08,
+                R.drawable.traffic_09, R.drawable.traffic_10, R.drawable.traffic_11,
+                R.drawable.traffic_12, R.drawable.traffic_13, R.drawable.traffic_14,
+                R.drawable.traffic_15, R.drawable.traffic_16, R.drawable.traffic_17,
+                R.drawable.traffic_18, R.drawable.traffic_19, R.drawable.traffic_20};
+                //เก็บค่าในตัวแปร array
+
+        MyAdapter objMyAdapter = new MyAdapter(MainActivity.this, intImage, strTitle);
+        trafficListView.setAdapter(objMyAdapter);
 
 
     }//createListView
