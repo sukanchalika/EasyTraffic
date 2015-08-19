e51933ef29fcc1964876b89af35a4c96e762ee53
Sukan <ajsukan@gmail.com>
2015-08-19 14:47:13 +0700

    before afternoon break day3

--- app/src/main/java/pbru/infotech/sukanchalika/easytraffic/TestActivity.java
+++ app/src/main/java/pbru/infotech/sukanchalika/easytraffic/TestActivity.java
@@ -4,13 +4,72 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class TestActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView questionTextView;
+    private ImageView trafficImageView;
+    private RadioGroup choiceRadioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton;
+    private String[] questionStrings;
+    private int[] imageInts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+
+        //Bind Widget เป็นการ bind ตัวแปรกับ widget
+        bindWidget(); //จะเกิดตัวแดง กด Alt+enter
+
+    } //onCreate
+
+    //การตั้ง method โดยใช้ override โดยให้ไปเรียกการทำงานเริ่มต้นที่ onStart เริ่มแรก
+    @Override
+    protected void onStart() {
+
+        //Setup โดยการตึงตัวแปรจากตัวคำถาม 10 ตัวมาไว้ที่นี่
+        questionStrings = getResources().getStringArray(R.array.question);
+        //ดึงรูปภาพมาวาง
+        imageInts = new int[10];
+        imageInts[0] = R.drawable.traffic_01;
+        imageInts[1] = R.drawable.traffic_02;
+        imageInts[2] = R.drawable.traffic_03;
+        imageInts[3] = R.drawable.traffic_04;
+        imageInts[4] = R.drawable.traffic_05;
+        imageInts[5] = R.drawable.traffic_06;
+        imageInts[6] = R.drawable.traffic_07;
+        imageInts[7] = R.drawable.traffic_08;
+        imageInts[8] = R.drawable.traffic_09;
+        imageInts[9] = R.drawable.traffic_10;
+
+        String[] strChoice = getResources().getStringArray(R.array.times1);
+
+        //Just Start
+        questionTextView.setText(questionStrings[0]);
+        trafficImageView.setImageResource(imageInts[0]);
+        choice1RadioButton.setText(strChoice[0]);
+        choice2RadioButton.setText(strChoice[1]);
+        choice3RadioButton.setText(strChoice[2]);
+        choice4RadioButton.setText(strChoice[3]);
+
+        super.onStart();
+    }
+
+    private void bindWidget() {
+        questionTextView = (TextView) findViewById(R.id.txtQuestion);
+        trafficImageView = (ImageView) findViewById(R.id.imvTrafficTest);
+        choiceRadioGroup = (RadioGroup) findViewById(R.id.ragChoice);
+        choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
+        choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
+        choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
+        choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
+
     }
 
     @Override
@@ -34,4 +93,4 @@ public class TestActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+} //Main Class
